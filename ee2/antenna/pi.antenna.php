e85469056563df9d057f7914d48099e088209d6a
Michael Gilley <michaelgilley@MikesMac.local>
2011-01-06 16:15:31 -0800

    Updating errant reference to PATH_CACHE constant.

--- ee2/antenna/pi.antenna.php
+++ ee2/antenna/pi.antenna.php
@@ -174,7 +174,7 @@ class Antenna
 	{	
 		// Check for cache directory
 		
-		$dir = PATH_CACHE.$this->cache_name.'/';
+		$dir = APPPATH.'cache/'.$this->cache_name.'/';
 		
 		if ( ! @is_dir($dir))
 		{
@@ -226,7 +226,7 @@ class Antenna
 	{
 		// Check for cache directory
 		
-		$dir = PATH_CACHE.$this->cache_name.'/';
+		$dir = APPPATH.'cache/'.$this->cache_name.'/';
 
 		if ( ! @is_dir($dir))
 		{
